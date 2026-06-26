d69ba0d586350a104665d6827c01f8a5a04c6281
mtomar907 <mtomar907@student.fuhsd.org>
2026-06-26 12:42:27 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -56,12 +56,14 @@ current_time = current_data["current"]["time"]
 
 today = date.today()
 current_year = today.year
+temp_c = current_temp
+temp_f = round(temp_c * 9/5 + 32, 1)
 
 log_df = pd.DataFrame({
     "date": [str(today)],
     "time": [current_time],
-    "temperature_2m": [current_temp],
-    "temperature_2m_f": [current_temp * 9/5 + 32]
+    "temperature_2m": [temp_c],
+    "temp_f": [temp_f]
 })
 log_file = "daily_log.csv"
 log_df.to_csv(log_file, mode='a', header=not os.path.isfile(log_file), index=False)
